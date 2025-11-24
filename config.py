ee7e3aa52a9ec87c86222e615b0bcc90d91c4946
AkshatMekol <83553564+AkshatMekol@users.noreply.github.com>
2025-11-24 14:35:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ BATCH_SIZE = 5000
 NUM_WORKERS_DEEPSEEK = 50
 NUM_WORKERS_OLA = 20
 
-JSONL_FILE = "/content/tender.jsonl"
+JSONL_FILE = "/home/ubuntu/TenderScraper/TenderData/Tenders/23_Nov_2025/tender.jsonl"
 API_KEY = "sk-7bed13f3db394798955a76a39b2d1f2f"
 
 CENTRAL_URLS = [
